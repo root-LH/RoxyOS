d039be02899941398a14180b534435a43f66a0a8
root-LH <iizuna@naver.com>
2026-07-20 16:39:29 +0900

    feat: add basic shell commands and improve command parsing

--- kernel/shell.c
+++ kernel/shell.c
@@ -1,6 +1,7 @@
 #include "include/shell.h"
 #include "include/printk.h"
 #include "include/string.h"
+#include "include/vga.h"
 
 void shell_init(void)
 {
@@ -9,10 +10,42 @@ void shell_init(void)
 
 void shell_print_prompt(void)
 {
-    printk("myOS> ");
+    printk("RoxyOS> ");
 }
 
 void shell_execute(const char *cmd)
 {
-    (void)cmd;
+    char command[32];
+    int i = 0;
+
+    while (cmd[i] && cmd[i] != ' ' && i < (int)sizeof(command) - 1)
+    {
+        command[i] = cmd[i];
+        i++;
+    }
+
+    command[i] = '\0';
+
+    const char *args = cmd;
+
+    while (*args && *args != ' ')
+    {
+        args++;
+    }
+
+    if (strcmp(command, "help") == 0)
+    {
+        printk("Commands:\n");
+        printk("  help  - Show available commands\n");
+        printk("  clear - Clear the screen\n");
+        return;
+    }
+
+    if (strcmp(command, "clear") == 0)
+    {
+        vga_clear();
+        return;
+    }
+
+    printk("Unknown command\n");
 }

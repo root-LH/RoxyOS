eef2bcb89bdfb1d6b6772e4fc9be216097e3ed0f
root-LH <iizuna@naver.com>
2026-07-22 19:09:09 +0900

    feat: implement reusable kernel heap allocator

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -7,6 +7,12 @@
 #define KERNEL_HEAP_START 0x100000
 #define KERNEL_HEAP_SIZE  (1024 * 1024)
 
+typedef struct block_header
+{
+    size_t size;
+    uint8_t free;
+} block_header_t;
+
 void memory_init(void);
 void *kmalloc(size_t size);
 void kfree(void *ptr);
--- kernel/libk/memory.c
+++ kernel/libk/memory.c
@@ -8,6 +8,24 @@ static size_t align4(size_t size)
     return (size + 3) & ~3;
 }
 
+static block_header_t *find_free_block(size_t size)
+{
+    uint8_t *current = (uint8_t *)KERNEL_HEAP_START;
+
+    while (current < heap_ptr)
+    {
+        block_header_t *header = (block_header_t *)current;
+
+        if (header->free && header->size >= size)
+            return header;
+
+        current += sizeof(block_header_t);
+        current += header->size;
+    }
+
+    return NULL;
+}
+
 void memory_init(void)
 {
     heap_ptr = (uint8_t *)KERNEL_HEAP_START;
@@ -20,9 +38,23 @@ void *kmalloc(size_t size)
 
     size = align4(size);
 
-    if (heap_ptr + size > heap_end)
+    block_header_t *header = find_free_block(size);
+
+    if (header != NULL)
+    {
+        header->free = 0;
+        return (void *)(header + 1);
+    }
+
+    if (heap_ptr + sizeof(block_header_t) + size > heap_end)
         return NULL;
 
+    header = (block_header_t *)heap_ptr;
+
+    header->size = size;
+    header->free = 0;
+
+    heap_ptr += sizeof(block_header_t);
     void *ptr = heap_ptr;
     heap_ptr += size;
 
@@ -31,5 +63,11 @@ void *kmalloc(size_t size)
 
 void kfree(void *ptr)
 {
-    (void)ptr;
+    if (ptr == NULL)
+        return;
+
+    block_header_t *header =
+        (block_header_t *)ptr - 1;
+
+    header->free = 1;
 }

0e7807bb993289e2fe121613ec7edda3da0d55eb
root-LH <iizuna@naver.com>
2026-07-22 19:25:13 +0900

    feat: implement kernel heap allocator with block splitting and coalescing

--- kernel/libk/memory.c
+++ kernel/libk/memory.c
@@ -26,6 +26,51 @@ static block_header_t *find_free_block(size_t size)
     return NULL;
 }
 
+static void split_block(block_header_t *header, size_t size)
+{
+    size_t remaining = header->size - size;
+
+    if (remaining <= sizeof(block_header_t) + 4)
+        return;
+
+    block_header_t *new_block =
+        (block_header_t *)((uint8_t *)(header + 1) + size);
+
+    new_block->size = remaining - sizeof(block_header_t);
+    new_block->free = 1;
+
+    header->size = size;
+}
+
+static void coalesce_blocks(void)
+{
+    uint8_t *current = (uint8_t *)KERNEL_HEAP_START;
+
+    while (current < heap_ptr)
+    {
+        block_header_t *header = (block_header_t *)current;
+
+        uint8_t *next_addr =
+            current + sizeof(block_header_t) + header->size;
+
+        if (next_addr >= heap_ptr)
+            break;
+
+        block_header_t *next =
+            (block_header_t *)next_addr;
+
+        if (header->free && next->free)
+        {
+            header->size +=
+                sizeof(block_header_t) + next->size;
+
+            continue;
+        }
+
+        current = next_addr;
+    }
+}
+
 void memory_init(void)
 {
     heap_ptr = (uint8_t *)KERNEL_HEAP_START;
@@ -42,6 +87,7 @@ void *kmalloc(size_t size)
 
     if (header != NULL)
     {
+        split_block(header, size);
         header->free = 0;
         return (void *)(header + 1);
     }
@@ -70,4 +116,5 @@ void kfree(void *ptr)
         (block_header_t *)ptr - 1;
 
     header->free = 1;
+    coalesce_blocks();
 }
